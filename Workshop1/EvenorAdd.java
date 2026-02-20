e749dc34e8998964127da4cfaa48a2a782cb0bde
praveenm <praveenmpravee7242@gmail.com>
2026-02-20 08:02:31 +0530

    even or add

---
+++ Workshop1/EvenorAdd.java
@@ -0,0 +1,13 @@
+public class EvenorAdd{
+    static void find(int num){
+        if(num%2==0){
+            System.out.println("even");
+            }
+            else{
+            System.out.println("odd");
+    }
+    }
+ public static void main(String args[]){
+    find(10);
+}
+    }
