8c8e8d2941fde2c48604bc57d908862ce3a0fea9
praveenm <praveenmpravee7242@gmail.com>
2026-02-25 22:30:25 +0530

    updated

--- Workshop1/Table.java
+++ Workshop1/Table.java
@@ -8,9 +8,18 @@ public class Table {
         System.out.println(num + "*" +i +"=" +result);
         i++;
     }
-    
     }
+    static void tabelss(int num){
+    for(int i=1;i<=10;i++){
+        int result=num*i;
+        System.out.println(num + "*" +i +"=" +result);
+    }
+
+}
+    
+    
     public static void main(String args[]){
         tabels(10);
+        tabelss(5);
     }
 }
